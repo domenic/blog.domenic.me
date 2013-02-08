354957a808d347b12311c186c1dee49dc6f06661
Domenic Denicola <domenic@domenicdenicola.com>
2013-02-08 01:15:19 -0500

    Don't add line numbers to code blocks.

--- plugins/pygments_code.rb
+++ plugins/pygments_code.rb
@@ -12,7 +12,7 @@ module HighlightCode
     lang = 'perl' if lang == 'pl'
     lang = 'yaml' if lang == 'yml'
     str = pygments(str, lang).match(/<pre>(.+)<\/pre>/m)[1].to_s.gsub(/ *$/, '') #strip out divs <div class="highlight">
-    tableize_code(str, lang)
+    wrap_code(str, lang)
   end
 
   def pygments(code, lang)
@@ -29,13 +29,7 @@ module HighlightCode
     end
     highlighted_code
   end
-  def tableize_code (str, lang = '')
-    table = '<div class="highlight"><table><tr><td class="gutter"><pre class="line-numbers">'
-    code = ''
-    str.lines.each_with_index do |line,index|
-      table += "<span class='line-number'>#{index+1}</span>\n"
-      code  += "<span class='line'>#{line}</span>"
-    end
-    table += "</pre></td><td class='code'><pre><code class='#{lang}'>#{code}</code></pre></td></tr></table></div>"
+  def wrap_code (str, lang = '')
+    "<pre class='highlight'><code class='#{lang}'>#{str}</code></pre>"
   end
 end
